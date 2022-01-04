28e0f183e8b45c43a25f403a336712be83b944cb
SSG\212573 <jhlee9091@gmail.com>
2022-01-04 15:08:04 +0900

    220104_REST API
    - Restful method for comments
    - Gson serializer, Ajax

---
+++ Board/src/main/java/com/board/controller/CommentController.java
@@ -0,0 +1,46 @@
+package com.board.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.board.adapter.GsonLocalDateTimeAdapter;
+import com.board.domain.CommentDTO;
+import com.board.service.CommentService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+@RestController
+public class CommentController {
+	
+	@Autowired
+	private CommentService commentService;
+	
+	@GetMapping(value = "/comments/{boardIdx}")				// @PathVariable 지정 변수와 바인딩
+	public JsonObject getCommentList(@PathVariable("boardIdx") Long boardIdx, @ModelAttribute("params") CommentDTO params) {
+		
+		// JSON 객체 생성
+		JsonObject jsonObj = new JsonObject();
+		
+		List<CommentDTO> commentList = commentService.getCommentList(params);
+		if (CollectionUtils.isEmpty(commentList) == false) {
+			
+			// 어댑터 클래스 포함하여 JsonArrary 타입으로 변환
+			Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new GsonLocalDateTimeAdapter()).create();
+			JsonArray jsonArr = gson.toJsonTree(commentList).getAsJsonArray();
+			
+			// 객체에 프로퍼티 추가
+			jsonObj.add("commentList", jsonArr);
+		}
+		
+		return jsonObj;
+	}
+}
